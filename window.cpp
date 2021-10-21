b825e7d2817f14c67d6e23160126e8e8530c7350
sarrio <samarixyt@gmail.com>
2021-10-21 18:02:48 -0400

    added window dragging

--- window.cpp
+++ window.cpp
@@ -1,5 +1,7 @@
 #include <SDL2/SDL.h>
-#include "SDL2/SDL_main.h"
+#include <SDL2/SDL_main.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
 #include <stdio.h>
 #include <vector>
 #include <algorithm>
@@ -9,15 +11,22 @@ bool XYInRect(const SDL_Rect& rect, int x, int y)
     return ((x >= rect.x && x <= rect.x + rect.w) && (y >= rect.y && y <= rect.y + rect.h));
 }
 
-std::vector<SDL_Event>& GetEvents() {
-    static std::vector<SDL_Event> frameEvents;
-    return frameEvents;
+SDL_Rect titleBar;
+
+SDL_HitTestResult hitCallback(SDL_Window* win, const SDL_Point* area, void* data) {
+    if (XYInRect(titleBar, area->x, area->y)) {
+        return (SDL_HITTEST_DRAGGABLE);
+    }
 }
+
+std::vector<SDL_Event> eventVector;
 
 int main(int argc, char* args[]) {
     bool quit = false;
+
     int winW = 1300;
     int winH = 700;
+
     Uint32 flags = SDL_WINDOW_BORDERLESS | SDL_WINDOW_RESIZABLE | SDL_WINDOW_OPENGL;
     SDL_Window* window = SDL_CreateWindow("srcpool", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, winW, winH, flags);
 
@@ -33,41 +42,24 @@ int main(int argc, char* args[]) {
         SDL_SetRenderDrawColor(renderer, 28, 28, 29, 255);
         SDL_RenderClear(renderer);
 
-        SDL_Rect titleBar;
         titleBar.x = 0;
         titleBar.y = 0;
         titleBar.w = winW;
         titleBar.h = 20;
 
-        SDL_SetRenderDrawColor(renderer, 33, 33, 34, 255);
+        SDL_SetRenderDrawColor(renderer, 26, 26, 27, 255);
         SDL_RenderFillRect(renderer, &titleBar);
         SDL_RenderPresent(renderer);
         
-
         // Input Collection
         while (SDL_PollEvent(&event) != 0) {
-            GetEvents().push_back(event);
             if (event.type == SDL_QUIT) {
                 quit = true;
             }
+            // TODO: Add event for clicking on resize frame, then set the window size to new size when dragging.
         }
 
-        // TODO: Add event for clicking on resize frame, then set the window size to new size when dragging.
-
-        bool drag = false;
-
-        if (std::binary_search(GetEvents().begin(), GetEvents().end(), SDL_MOUSEBUTTONDOWN)) {
-            drag = true;
-        }
-        else if (std::binary_search(GetEvents().begin(), GetEvents().end(), SDL_MOUSEBUTTONUP)) {
-            drag = false;
-        }
-
-        if (XYInRect(titleBar, mouseX, mouseY) && drag) {
-            SDL_SetWindowPosition(window, mouseX, mouseY);
-        }
-
-        GetEvents().clear();
+        SDL_SetWindowHitTest(window, hitCallback, 0);
     }
 
     SDL_DestroyWindow(window);
